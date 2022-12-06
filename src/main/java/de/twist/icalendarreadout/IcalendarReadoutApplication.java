178be480dfc187bd0702eb7344048a92fac6c4a8
Oliver <oliver.kuhn@ancud.de>
2022-12-06 14:53:08 +0100

    project init

---
+++ src/main/java/de/twist/icalendarreadout/IcalendarReadoutApplication.java
@@ -0,0 +1,13 @@
+package de.twist.icalendarreadout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcalendarReadoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcalendarReadoutApplication.class, args);
+	}
+
+}
